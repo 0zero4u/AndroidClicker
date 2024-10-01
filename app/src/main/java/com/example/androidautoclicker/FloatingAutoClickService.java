5036328aa3f93dfe93dead27060c4f90fe6eaba3
Heck <97088365+0zero4u@users.noreply.github.com>
2024-10-02 01:08:42 +0530

    Update FloatingAutoClickService.kt

--- app/src/main/java/com/example/androidautoclicker/FloatingAutoClickService.java
+++ app/src/main/java/com/example/androidautoclicker/FloatingAutoClickService.java
@@ -1,179 +1,148 @@
-package com.example.androidautoclicker;
+package com.example.androidautoclicker
 
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.PixelFormat;
-import android.os.Build;
-import android.os.IBinder;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.annotation.Nullable
 
-public class FloatingAutoClickService extends Service {
-    private WindowManager manager;
-    private View view;
-    private TextView floatingTextView;
-    private WindowManager.LayoutParams params;
-    private int xForRecord = 0;
-    private int yForRecord = 0;
-    private int startDragDistance = 0;
-    private MyAccessibilityService autoClickService = MyAccessibilityService.getInstance();
-    private TouchAndDragListener touchAndDragListener;
+object FloatingAutoClickService : Service() {
+    private var manager: WindowManager? = null
+    private var view: View? = null
+    private var floatingTextView: TextView? = null
+    private var params: WindowManager.LayoutParams? = null
+    private var xForRecord = 0
+    private var yForRecord = 0
+    private var startDragDistance = 0
+    private var touchAndDragListener: TouchAndDragListener? = null
 
-    private enum ClickMode {
+    private enum class ClickMode {
         CLICK, SWIPE
     }
-    private ClickMode currentMode = ClickMode.CLICK;
 
+    private var currentMode = ClickMode.CLICK
 
     @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        view = LayoutInflater.from(this).inflate(R.layout.floating_widget, null);
-        floatingTextView = view.findViewById(R.id.floatingTextView);
+    override fun onCreate() {
+        super.onCreate()
+        view = LayoutInflater.from(this).inflate(R.layout.floating_widget, null)
+        floatingTextView = view?.findViewById(R.id.floatingTextView)
 
-        int overlayParam;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            overlayParam = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        val overlayParam = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
         } else {
-            overlayParam = WindowManager.LayoutParams.TYPE_PHONE;
+            WindowManager.LayoutParams.TYPE_PHONE
         }
 
-        params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                overlayParam,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT
-        );
+        params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            overlayParam,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        )
 
-        manager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        if (manager != null) {
-            manager.addView(view, params);
-        }
+        manager = getSystemService(Context.WINDOW_SERVICE) as WindowManager?
+        manager?.addView(view, params)
 
-        touchAndDragListener = new TouchAndDragListener(params, startDragDistance,
-                () -> switchMode(),
-                () -> manager.updateViewLayout(view, params));
-        view.setOnTouchListener(touchAndDragListener);
+        touchAndDragListener = TouchAndDragListener(params, startDragDistance,
+            onTouch = { switchMode() },
+            onDrag = { manager?.updateViewLayout(view, params) })
+
+
+        view?.setOnTouchListener(touchAndDragListener)
     }
 
-    private void switchMode() {
-        currentMode = (currentMode == ClickMode.CLICK) ? ClickMode.SWIPE : ClickMode.CLICK;
-        floatingTextView.setText(currentMode == ClickMode.CLICK ? "CLICK" : "SWIPE");
+    private fun switchMode() {
+        currentMode = if (currentMode == ClickMode.CLICK) ClickMode.SWIPE else ClickMode.CLICK
+        floatingTextView?.text = if (currentMode == ClickMode.CLICK) "CLICK" else "SWIPE"
     }
 
-    private void performAction(float x, float y) {
-        switch (currentMode) {
-            case CLICK:
-                autoClickService.autoClick(0, 10, (int) x, (int) y);
-                break;
-            case SWIPE:
-                autoClickService.autoSwipe(0, 500, (int) x, (int) y, (int) x + 100, (int) y);
-                break;
+    private fun performAction(x: Float, y: Float) {
+        when (currentMode) {
+            ClickMode.CLICK -> MyAccessibilityService.autoClick(0, 10, x.toInt(), y.toInt())
+            ClickMode.SWIPE -> MyAccessibilityService.autoSwipe(0, 500, x.toInt(), y.toInt(), x.toInt() + 100, y.toInt())
         }
     }
 
 
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (manager != null && view != null) {
-            manager.removeView(view);
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        manager?.removeView(view)
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        int x = params.x;
-        int y = params.y;
-        params.x = xForRecord;
-        params.y = yForRecord;
-        xForRecord = x;
-        yForRecord = y;
-        if (manager != null) {
-            manager.updateViewLayout(view, params);
-        }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val x = params?.x ?: 0
+        val y = params?.y ?: 0
+        params?.x = xForRecord
+        params?.y = yForRecord
+        xForRecord = x
+        yForRecord = y
+        manager?.updateViewLayout(view, params)
     }
 
-    private class TouchAndDragListener implements View.OnTouchListener {
-        private final WindowManager.LayoutParams params;
-        private final int startDragDistance;
-        private final Runnable onTouch;
-        private final Runnable onDrag;
-        private int initialX;
-        private int initialY;
-        private float initialTouchX;
-        private float initialTouchY;
-        private boolean isDrag = false;
+    private inner class TouchAndDragListener(
+        private val params: WindowManager.LayoutParams?,
+        private val startDragDistance: Int,
+        private val onTouch: Runnable?,
+        private val onDrag: Runnable?
+    ) : View.OnTouchListener {
+        private var initialX = 0
+        private var initialY = 0
+        private var initialTouchX = 0f
+        private var initialTouchY = 0f
+        private var isDrag = false
 
-
-        public TouchAndDragListener(WindowManager.LayoutParams params, int startDragDistance, Runnable onTouch, Runnable onDrag) {
-            this.params = params;
-            this.startDragDistance = startDragDistance;
-            this.onTouch = onTouch;
-            this.onDrag = onDrag;
+        private fun isDragging(event: MotionEvent): Boolean {
+            val dx = event.rawX - initialTouchX
+            val dy = event.rawY - initialTouchY
+            return (dx * dx + dy * dy) > startDragDistance * startDragDistance
         }
 
-        private boolean isDragging(MotionEvent event) {
-            double dx = event.getRawX() - initialTouchX;
-            double dy = event.getRawY() - initialTouchY;
-            return (Math.pow(dx, 2) + Math.pow(dy, 2)) > Math.pow(startDragDistance, 2);
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            switch (event.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN:
-                    isDrag = false;
-                    initialX = params.x;
-                    initialY = params.y;
-                    initialTouchX = event.getRawX();
-                    initialTouchY = event.getRawY();
-                    return true;
-
-                case MotionEvent.ACTION_MOVE:
+        override fun onTouch(v: View, event: MotionEvent): Boolean {
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    isDrag = false
+                    initialX = params?.x ?: 0
+                    initialY = params?.y ?: 0
+                    initialTouchX = event.rawX
+                    initialTouchY = event.rawY
+                    return true
+                }
+                MotionEvent.ACTION_MOVE -> {
                     if (!isDrag && isDragging(event)) {
-                        isDrag = true;
+                        isDrag = true
                     }
-                    if (!isDrag) return true;
-                    params.x = initialX + (int) (event.getRawX() - initialTouchX);
-                    params.y = initialY + (int) (event.getRawY() - initialTouchY);
-                    if (onDrag != null) {
-                        onDrag.run();
-                    }
-                    return true;
-
-                case MotionEvent.ACTION_UP:
+                    if (!isDrag) return true
+                    params?.x = initialX + (event.rawX - initialTouchX).toInt()
+                    params?.y = initialY + (event.rawY - initialTouchY).toInt()
+                    onDrag?.run()
+                    return true
+                }
+                MotionEvent.ACTION_UP -> {
                     if (!isDrag) {
-                        performAction(event.getRawX(), event.getRawY());
-                        return true;
+                        performAction(event.rawX, event.rawY)
+                        onTouch?.run() // Call onTouch for clicks
+                        return true
                     }
-                    break;
+
+                }
             }
-            return false;
+            return false
         }
-
-
-        public float getInitialTouchY() { return initialTouchY; }
-        public float getInitialTouchX() { return initialTouchX; }
-        public int getInitialY() { return initialY;  }
-        public int getInitialX() { return initialX; }
     }
-
-
 }
